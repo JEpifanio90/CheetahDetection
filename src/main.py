2c2dd19e396654c47ae359dad24a39f1c8e9caa9
Jose Epifanio <jose.epifanio90@gmail.com>
2019-06-02 20:40:51 -0400

    Did something

--- src/main.py
+++ src/main.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 from pathlib import Path
 from tkinter import Tk
-from tkinter import messagebox, Label
+from tkinter import Label, Button, StringVar, Canvas, Frame
 from PIL import Image, ImageTk
-"""Simple Image Classification. A bare bone Machine Learning implementation.
+'''Simple Image Classification. A bare bone Machine Learning implementation.
 
-Some issue alskdalskdl.
-"""
+Essentially this is going to compare two images and remove their colors
+by applying all sorts of filters. Next with the help of KNN and other
+algorithms we are going to calculate a percentage of similitude and
+display it to the user.
+
+NOTE: Everything was made without TensorFlow or any other ML/DL
+libraries
+'''
 __author__ = 'Jose Epifanio'
-__credits__ = ['Ludim Sanchez', 'Alberto Hernandez', 'Guillermo Gonzalez']
+__credits__ = [
+    'Ludim Sanchez',
+    'Alberto Hernandez',
+    'Guillermo Gonzalez',
+    'Jose Epifanio'
+]
 __licence__ = 'MIT'
-__version__ = "1.0.0"
-__email__ = "jose.epifanio90@gmail.com"
-__status__ = "Development"
+__version__ = '1.0.0'
+__email__ = 'jose.epifanio90@gmail.com'
+__status__ = 'Development'
 
 
 class Main():
@@ -26,27 +37,78 @@ class Main():
     def set_main_container(self):
         icon = Image.open('./img/icon.png')
         icon = ImageTk.PhotoImage(icon)
-        self.window_container.tk.call('wm', 'iconphoto', self.window_container._w, icon)
+        self.window_container.tk.call('wm', 'iconphoto', self.window_container._w, icon)  # noqa
         self.window_container.title('Cheetah Detection')
-        self.set_background()
         self.center_window()
+        self.set_ui()
 
     def center_window(self):
         self.window_container.update_idletasks()
-        w = self.window_container.winfo_width()
-        h = self.window_container.winfo_height()
+        w, h = 1200, 350
         x = (self.window_container.winfo_screenwidth() // 2) - (w // 2)
         y = (self.window_container.winfo_screenheight() // 2) - (h // 2)
-        self.window_container.geometry('{}x{}+{}+{}'.format(w, h, x, y))
+        self.window_container.geometry(f'{w}x{h}+{x}+{y}')
 
-    def set_background(self):
-        bg_image = Image.open('./img/background.png')
-        bg_image = ImageTk.PhotoImage(bg_image)
-        img_background = Label(self.window_container, image=bg_image)
-        img_background.image = bg_image
-        img_background.pack()
-        img_background.grid(row=0, column=0)
+    def set_ui(self):
+        self.add_frames()
+        self.add_label_titles()
+        self.add_img_frames()
+        self.add_buttons()
 
-if __name__ == "__main__":
+    def add_frames(self):
+        self.header_frame = Frame(self.window_container)
+        self.header_frame.pack(side='top', fill='both')
+        self.left_frame = Frame(self.window_container)
+        self.left_frame.pack(side='left', fill='both', padx=65)
+        self.right_frame = Frame(self.window_container)
+        self.right_frame.pack(side='right', fill='both', padx=55)
+        self.btm_frame = Frame(self.window_container)
+        self.btm_frame.pack(side='bottom', fill='both')
+
+    def add_label_titles(self):
+        usr_lbl_title = StringVar()
+        usr_lbl = Label(
+            self.header_frame,
+            textvariable=usr_lbl_title,
+            relief='flat'
+        )
+        usr_lbl_title.set('Your selected Image')
+        usr_lbl.pack(side='left', fill='both', padx=220)
+
+        out_lbl_title = StringVar()
+        out_lbl = Label(
+            self.header_frame,
+            textvariable=out_lbl_title,
+            relief='flat'
+        )
+        out_lbl_title.set('Output Image')
+        out_lbl.pack(side='right', fill='both', padx=220)
+
+    def add_img_frames(self):
+        usr_canva = Canvas(self.left_frame, bg='red', relief='raised')
+        usr_canva.pack(fill='both', padx=20, pady=50)
+
+        out_canva = Canvas(
+            self.right_frame,
+            bg='blue',
+            relief='raised'
+        )
+        out_canva.pack(fill='both', padx=20, pady=50)
+
+    def add_buttons(self):
+        load_btn = Button(self.btm_frame, text='Load Image', command=self.btn_callback)
+        load_btn.pack(side='left', padx=25)
+        snip_btn = Button(self.btm_frame, text='Snip Image', command=self.btn_callback)
+        snip_btn.pack(side='left', padx=25)
+        start_btn = Button(self.btm_frame, text='Start!', command=self.btn_callback)
+        start_btn.pack(side='left', padx=25)
+        cls_btn = Button(self.btm_frame, text='Clear', command=self.btn_callback)
+        cls_btn.pack(side='left', padx=25)
+
+    def btn_callback(self):
+        print('OUCH!')
+
+
+if __name__ == '__main__':
     main_instance = Main()
     main_instance.init_view()
